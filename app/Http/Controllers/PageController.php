31a4c3fa5a34ca1f856c249b91dda7de3cc8bf34
Gabija <gabija.arm@gmail.com>
2021-04-26 16:42:21 +0300

    Initial commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Page;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+        $title = 'Welcome, to LARA-b an it-blog';
+        return view('pages.about')->with('title', $title);
+    }
+
+    public function services()
+    {
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design',  'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
+    }
+
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('pages.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Page  $page
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Page $page)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Page  $page
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Page $page)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Page  $page
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Page $page)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Page  $page
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Page $page)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+    <h1> Welcome to IT-blog <h1>
+    <p> A blog about newest information technologies for science entusiasts <p>
+    </div>
+    @endsection
